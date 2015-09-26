eb4bba6a36db8d63e6cca876ad1255507d8feddc
FooP007 <FooP006@web.de>
2015-09-27 01:11:16 +0200

    Upgrading Project
    I made a first test to upgrade projects

---
+++ Assets/Scripts/Carpool.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Carpool : MonoBehaviour 
+{
+
+	public Sprite carpool1;
+	public Sprite carpool2;
+	public Sprite carpool3;
+	public Sprite carpool4;
+	public Sprite carpool5;
+
+	private Sprite[] sprites;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		//this.gameObject.AddComponent<SpriteRenderer>();
+
+	}
+
+	void Start()
+	{
+		sprites = new Sprite[5] { carpool1, carpool2, carpool3, carpool4, carpool5 };
+	}
+
+	public void FillSpriteRenderer(int projectLevel)
+	{
+		SpriteRenderer appearence = this.GetComponent<SpriteRenderer>();
+		appearence.sortingLayerName = "Buildings";
+
+
+		if(projectLevel > 0 && projectLevel <= sprites.Length)
+		{
+			appearence.sprite = sprites[projectLevel-1];
+			Debug.Log (sprites[projectLevel-1]);
+		}
+		else
+		{
+			Debug.Log ("The given level was not in the expected range. Range :1 - " + sprites.Length +" but projectLevel was " + projectLevel);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour 
+{
+	public GameObject carpool;
+
+	private Carpool carpoolScript;
+	private int carpoolLevel = 0;
+
+	private bool spaceKeyDown = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		carpoolScript = carpool.GetComponent<Carpool>();
+
+		buildTown();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown("space"))
+		{
+
+			spaceKeyDown = true;
+			carpoolLevel++;
+			carpoolScript.FillSpriteRenderer(carpoolLevel);
+		}
+
+		if(Input.GetKeyUp("space")  )
+		{
+			spaceKeyDown = false;
+		}
+	}
+
+	void buildTown()
+	{
+
+		carpoolScript.FillSpriteRenderer(carpoolLevel);
+
+	}
+}
